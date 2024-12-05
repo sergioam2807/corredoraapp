fed35117e0b3d032b79b5b06c951a9eb44f16bb0
Sergio <sergioam2807@gmail.com>
2024-12-05 16:58:39 -0300

    fix: add card style

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -1,22 +1,33 @@
 'use client'
-import { Card, CardHeader, CardBody, Image } from '@nextui-org/react'
+import Image from 'next/image'
+import { Card, CardHeader, CardBody, CardFooter, Chip } from '@nextui-org/react'
+import dpto from '@/public/dpto1.jpg'
 
 export const CardComponent = () => {
   return (
     <Card className="py-4">
       <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="font-bold text-large">Frontend Radio</h4>
+        <small className="text-default-500">Departamento</small>
+        <h4 className="font-bold text-large">Cumbres del bosque</h4>
+        <small className="text-default-500">Viña del mar</small>
+        {/* <p className="text-tiny uppercase font-bold">Viña del mar</p> */}
       </CardHeader>
       <CardBody className="overflow-visible py-2">
         <Image
+          src={dpto}
           alt="Card background"
           className="object-cover rounded-xl"
-          src="https://nextui.org/images/hero-card-complete.jpeg"
-          width={270}
+          height={200}
+          width={300}
         />
       </CardBody>
+      <CardBody className="flex  px-4">
+        <div className="flex justify-between items-center">
+          <p className="uppercase font-bold">uf 12500</p>
+          <Chip color="default">Venta</Chip>
+        </div>
+      </CardBody>
+      <CardFooter className="flex justify-between items-center px-4"></CardFooter>
     </Card>
   )
 }
