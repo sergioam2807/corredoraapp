d6551e4df6da77b5ead133e353f2a7804df8e403
Sergio <sergioam2807@gmail.com>
2024-12-05 15:27:11 -0300

    fix: change to module config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import { nextui } from '@nextui-org/theme'
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
   },
-
   plugins: [nextui()],
 }
+
+export default config
