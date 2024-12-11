aab2c22e967d2e97d8713550c239f72c8797abbe
Sergio <sergioam2807@gmail.com>
2024-12-11 20:32:24 -0300

    feat: add auth0 sso

--- app/(admin)/admin/layout.tsx
+++ app/(admin)/admin/layout.tsx
@@ -1,9 +1,55 @@
 import '@/styles/globals.css'
+import { Metadata } from 'next'
+import clsx from 'clsx'
+import { siteConfig } from '@/config/site'
+import { fontSans } from '@/config/fonts'
+import { Providers } from '@/app/(user)/providers'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: '/favicon.ico',
+  },
+}
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <div>{children}</div>
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <title>{metadata.title.default}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} />
+      </head>
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <Providers
+          themeProps={{
+            attribute: 'class',
+            themes: ['light'],
+            defaultTheme: 'light',
+            storageKey: 'theme',
+          }}
+        >
+          <UserProvider>
+            <div className="relative flex flex-col h-screen">
+              <main className="pt-8 px-8">{children}</main>
+            </div>
+          </UserProvider>
+        </Providers>
+      </body>
+    </html>
+  )
 }
