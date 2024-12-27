12ee0ad4e36d3395a36d26bb03c8c86c8a8649a0
Sergio <sergioam2807@gmail.com>
2024-12-27 15:57:07 -0300

    fix: add bottom bar and widgetcard

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,10 +1,22 @@
 'use client'
+import { WidgetCard } from '@/components/WidgetCard'
+import { useUser } from '@auth0/nextjs-auth0/client'
 
 function AdminPage() {
+  const { user, error, isLoading } = useUser()
+  const name = user?.name?.split(' ')[0]
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
   return (
-    <div>
-      <h1 className="text-3xl font-extrabold">Hola! Lorena 😁</h1>
-      <p>Content for authenticated users only.</p>
+    <div className="w-full flex justify-center items-center flex-col gap-4">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-extrabold">Hola! {name} 😁</h1>
+        <p>¿Que deseas publicar hoy?</p>
+      </div>
+      <div>
+        <WidgetCard />
+      </div>
     </div>
   )
 }
