f1a8495e152d38d8b8c5c902fbbbb72c595646b8
Sergio <sergioam2807@gmail.com>
2025-01-02 16:50:12 -0300

    fix: screens sizes in tailwind.config

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -6,31 +6,31 @@ import React from 'react'
 export const Bottombar = () => {
   const { user } = useUser()
   return (
-    <div className="fixed bottom-0 left-0 right-0 block md:hidden">
-      <div className="px-8 h-full bg-roseGold bg-opacity-70 text-white flex  p-4 rounded-t-3xl">
-        <div className="flex justify-center items-center sm:blovk ">
+    <div className="fixed bottom-0 left-0 right-0 block md:hidden z-50">
+      <div className="px-8 h-full bg-roseGold  text-white flex p-4 rounded-t-3xl">
+        <div className="flex justify-center items-center sm:block">
           {user?.picture && (
             <Avatar className="w-10 h-10 text-large" src={user?.picture} />
           )}
         </div>
-        <nav className="flex gap-4  flex-grow justify-center items-center">
+        <nav className="flex gap-4 flex-grow justify-center items-center ">
           <a
             href="/admin"
-            className="flex items-center gap-2 p-2 hover:bg-gray-700 hover:bg-opacity-40 rounded"
+            className="flex items-center gap-2 p-2 hover:bg-sidebar rounded-xl font-bold"
           >
             <span>Publicar</span>
           </a>
           <a
             href="admin/mis-publicaciones"
-            className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded"
+            className="flex items-center gap-2 p-2 hover:bg-sidebar rounded-xl font-bold"
           >
             <span>Mis Publicaciones</span>
           </a>
         </nav>
-        <div className="flex justify-center items-center ">
+        <div className="flex justify-center items-center">
           <a
             href="/api/auth/logout"
-            className="flex items-center gap-2 p-2 hover:bg-gray-700 rounded"
+            className="flex items-center gap-2 p-2 hover:bg-sidebar rounded-xl font-bold"
           >
             <span>Cerrar Sesion</span>
           </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,20 @@ const config = {
       },
       colors: {
         roseGold: '#e0bfb8',
+        sidebar: '#D8D7D9',
+        sidebarText: '#592828',
+        adminPage: '#F5F5F5',
       },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(180deg, #bf7b75 0%, #e0bfb8 100%)',
+      },
+    },
+    screens: {
+      sm: '766px',
+      md: '766px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
   },
   plugins: [nextui()],
