517b96d2759c30481283aa9adf0d83f79d8c637a
sergio <sergio.alvarez@evalueserve.com>
2025-01-18 16:37:33 -0300

    fix: reconfig skeleton

--- components/skeleton/SkeletonAdmin.tsx
+++ components/skeleton/SkeletonAdmin.tsx
@@ -1,48 +1,45 @@
-import useMediaQuery from '@/hooks/useMediaQuery'
 import { Skeleton } from '@heroui/skeleton'
 import '@/styles/custom.css'
 
 export default function SkeletonAdmin() {
-  const isXL = useMediaQuery('(min-width: 1280px)')
-
-  if (isXL) {
-    return (
-      <div className="flex flex-col gap-3">
-        <div className="flex gap-4">
-          <Skeleton className="rounded-lg w-full">
-            <div className="h-44 rounded-lg bg-default-300" />
-          </Skeleton>
-          <Skeleton className="rounded-lg w-full">
-            <div className="h-44 rounded-lg bg-default-300" />
-          </Skeleton>
-          <Skeleton className="rounded-lg w-full">
-            <div className="h-44 rounded-lg bg-default-300" />
-          </Skeleton>
-        </div>
-        <Skeleton className="rounded-lg">
-          <div className="custom-height rounded-lg bg-default-300" />
-        </Skeleton>
-      </div>
-    )
-  }
-
   return (
     <div className="flex flex-col gap-3">
-      <Skeleton className="rounded-lg">
-        <div className="h-24 rounded-lg bg-default-300" />
+      <div className="flex-col sm:flex-row gap-4 hidden sm:flex sm:block">
+        <Skeleton className="rounded-lg w-full">
+          <div className="h-24 sm:h-44 rounded-lg bg-default-300" />
+        </Skeleton>
+        <Skeleton className="rounded-lg w-full">
+          <div className="h-24 sm:h-44 rounded-lg bg-default-300" />
+        </Skeleton>
+        <Skeleton className="rounded-lg w-full">
+          <div className="h-24 sm:h-44 rounded-lg bg-default-300" />
+        </Skeleton>
+      </div>
+      <Skeleton className="rounded-lg  sm:block md:hidden">
+        <div className="h-72 sm:h-96 rounded-lg bg-default-300" />
       </Skeleton>
-      <Skeleton className="rounded-lg">
-        <div className="h-72 rounded-lg bg-default-300" />
+      <Skeleton className="rounded-lg  hidden sm:block">
+        <div className="h-72 sm:h-96 rounded-lg bg-default-300" />
       </Skeleton>
-      <Skeleton className="rounded-lg">
-        <div className="h-72 rounded-lg bg-default-300" />
+      <Skeleton className="rounded-lg  hidden sm:block">
+        <div className="h-72 sm:h-72 rounded-lg bg-default-300" />
       </Skeleton>
-      <Skeleton className="rounded-lg">
-        <div className="h-16 rounded-lg bg-default-300" />
-      </Skeleton>
-      <Skeleton className="rounded-lg">
-        <div className="h-16 rounded-lg bg-default-300" />
-      </Skeleton>
+      <div className="sm:hidden">
+        <Skeleton className="rounded-lg">
+          <div className="h-72 sm:h-96 rounded-lg bg-default-300" />
+        </Skeleton>
+      </div>
+      <div className="sm:hidden">
+        <Skeleton className="rounded-lg">
+          <div className="h-16 sm:h-20 rounded-lg bg-default-300" />
+        </Skeleton>
+      </div>
+
+      <div className="sm:hidden">
+        <Skeleton className="rounded-lg">
+          <div className="h-16 sm:h-20 rounded-lg bg-default-300" />
+        </Skeleton>
+      </div>
     </div>
   )
 }
