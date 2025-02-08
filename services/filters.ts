516d984ceb6f4c9093002f8d888183bfc7c5a796
sergio <sergio.alvarez@evalueserve.com>
2025-02-08 02:01:03 -0300

    fix: remove localhost error

--- services/filters.ts
+++ services/filters.ts
@@ -1,19 +1,23 @@
 export async function getFilters() {
-  const res = await fetch('http://localhost:3000/api/filters', {
+  const res = await fetch('/api/filters', {
     cache: 'no-store',
   })
+
   if (!res.ok) {
     throw new Error('Failed to fetch filters')
   }
+
   return res.json()
 }
 
 export async function getProperties() {
-  const res = await fetch('http://localhost:3000/api/properties', {
+  const res = await fetch('/api/properties', {
     cache: 'no-store',
   })
+
   if (!res.ok) {
     throw new Error('Failed to fetch properties')
   }
+
   return res.json()
 }
