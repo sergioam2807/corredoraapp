0701326e3c24d423359452d3ac44818925c42c2a
sergio <sergio.alvarez@evalueserve.com>
2025-03-07 18:07:49 -0300

    fix: update image previews in formprperties

--- components/FormProperties.tsx
+++ components/FormProperties.tsx
@@ -118,7 +118,8 @@ export const FormProperties: React.FC<FormPropertiesProps> = ({
             estadoVenta: data.disponibilidad_id || '',
             profitPercentage: data.profit_percentage || '',
             imagenes: [],
-            imagenesPreview: [],
+            imagenesPreview:
+              data.images.map((image: { url: string }) => image.url) || [],
             profit_percentage: data.profit_percentage || 0,
           })
         } catch (error) {
