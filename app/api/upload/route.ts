8c395cbd33b4634b9b43d6db0f6647bff28605b1
sergio <sergio.alvarez@evalueserve.com>
2025-03-21 16:39:55 -0300

    fix: resolve maintainer page test

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import { Storage } from '@google-cloud/storage'
 import { v4 as uuidv4 } from 'uuid'
 
@@ -16,49 +16,74 @@ const storage = new Storage({
   },
 })
 
-const bucketName: string = process.env.GCP_BUCKET_NAME || ''
+const bucketName = process.env.GCP_BUCKET_NAME
 
 if (!bucketName) {
   throw new Error('GCP_BUCKET_NAME environment variable is not set')
 }
 
-// export const config = {
-//   api: {
-//     bodyParser: {
-//       sizeLimit: '20mb',
-//     },
-//   },
-// }
+const bucket = storage.bucket(bucketName)
 
-export async function GET(req: Request) {
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '50mb',
+    },
+  },
+}
+
+export const POST = async (req: NextRequest) => {
   try {
-    const { searchParams } = new URL(req.url)
-    const fileType = searchParams.get('fileType') // ej: image/png
+    const formData = await req.formData()
+    const files = formData.getAll('file') as File[]
 
-    if (!fileType) {
-      return NextResponse.json(
-        { error: 'No fileType provided' },
-        { status: 400 }
-      )
+    if (files.length > 0) {
+      const urls: string[] = []
+
+      const uploadPromises = files.map(async (file) => {
+        const arrayBuffer = await file.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
+        const fileName = `${uuidv4()}-${file.name}`
+        const blob = bucket.file(fileName)
+        const blobStream = blob.createWriteStream({
+          resumable: false,
+        })
+
+        return new Promise<void>((resolve, reject) => {
+          blobStream.on('error', (err) => {
+            console.error('Stream error:', err)
+            reject(err)
+          })
+
+          blobStream.on('finish', () => {
+            const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+
+            urls.push(publicUrl)
+            resolve()
+          })
+
+          blobStream.end(buffer)
+        })
+      })
+
+      await Promise.all(uploadPromises)
+
+      return NextResponse.json({
+        success: true,
+        urls,
+      })
+    } else {
+      return NextResponse.json({
+        success: false,
+        error: 'No files uploaded',
+      })
     }
-
-    const fileName = `${uuidv4()}.${fileType.split('/')[1]}`
-    const bucket = storage.bucket(bucketName)
-    const file = bucket.file(fileName)
-
-    const [url] = await file.getSignedUrl({
-      action: 'write',
-      expires: Date.now() + 15 * 60 * 1000, // 15 minutos
-      contentType: fileType,
-    })
-
-    return NextResponse.json({ url, fileName })
   } catch (error) {
-    console.error('Error generating signed URL:', error)
+    console.error('Error in POST handler:', error)
 
-    return NextResponse.json(
-      { error: 'Failed to generate signed URL' },
-      { status: 500 }
-    )
+    return NextResponse.json({
+      success: false,
+      error: 'Error processing request',
+    })
   }
 }
