8770c0a0b3f4f974fd11a3aae92ee9ad9183fd7f
sergio <sergio.alvarez@evalueserve.com>
2025-03-29 15:19:50 -0300

    fix: change credential api key

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,25 +2,22 @@ import { NextRequest, NextResponse } from 'next/server'
 import { Storage } from '@google-cloud/storage'
 import { v4 as uuidv4 } from 'uuid'
 
-const storage = new Storage({
-  projectId: process.env.GCP_PROJECT_ID,
-  credentials: {
-    type: process.env.GCP_TYPE,
-    project_id: process.env.GCP_PROJECT_ID,
-    private_key_id: process.env.GCP_PRIVATE_KEY_ID,
-    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    client_email: process.env.GCP_CLIENT_EMAIL,
-    client_id: process.env.GCP_CLIENT_ID,
-    token_url: process.env.GCP_TOKEN_URI,
-    universe_domain: process.env.GCP_UNIVERSE_DOMAIN,
-  },
-})
+const serviceAccount = {
+  type: process.env.GCP_TYPE,
+  project_id: process.env.GCP_PROJECT_ID,
+  private_key_id: process.env.GCP_PRIVATE_KEY_ID,
+  private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.GCP_CLIENT_EMAIL,
+  client_id: process.env.GCP_CLIENT_ID,
+  auth_uri: process.env.GCP_AUTH_URI,
+  token_uri: process.env.GCP_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GCP_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GCP_CLIENT_X509_CERT_URL,
+}
 
-console.log('Private Key:', process.env.GCP_PRIVATE_KEY)
-console.log('Project ID:', process.env.GCP_PROJECT_ID)
-console.log('Client Email:', process.env.GCP_CLIENT_EMAIL)
-console.log('Bucket Name:', process.env.GCP_BUCKET_NAME)
-console.log('Storage:', storage)
+const storage = new Storage({
+  credentials: serviceAccount,
+})
 
 const bucketName = process.env.GCP_BUCKET_NAME
 
