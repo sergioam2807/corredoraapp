2e4432792fbe8f6a15b6492e81dbeb3c52076658
sergio <sergio.alvarez@evalueserve.com>
2025-04-24 23:41:57 -0400

    fix: add route.ts api validation when auth is denied

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -55,7 +55,6 @@ function AdminPageContent() {
           },
           body: JSON.stringify(formData),
         })
-        // const result = await response.json()
 
         if (response.ok) {
           setShowPopup(true)
--- app/api/auth/[...auth0]/route.ts
+++ app/api/auth/[...auth0]/route.ts
@@ -1,4 +1,21 @@
 import { handleAuth } from '@auth0/nextjs-auth0'
 
-export const GET = handleAuth()
+export const GET = handleAuth({
+  onError(req: unknown, error: unknown) {
+    if (error?.toString().includes('access_denied')) {
+      console.error('Access denied: Este email no está autorizado')
+
+      return new Response(null, {
+        status: 302,
+        headers: { Location: '/' },
+      })
+    }
+
+    return new Response(null, {
+      status: 500,
+      headers: { Location: '/' },
+    })
+  },
+})
+
 export const POST = handleAuth()
